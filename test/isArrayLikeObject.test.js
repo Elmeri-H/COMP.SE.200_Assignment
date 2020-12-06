aa7a19eea4ac868d3538240b94bc4690df87cc13
Valtteri Lahti <valtteri.lahti@gmail.com>
2020-12-06 15:35:46 +0200

    fix one test

--- test/isArrayLikeObject.test.js
+++ test/isArrayLikeObject.test.js
@@ -25,9 +25,4 @@ describe("isArrayLikeObject", () => {
     it("returns false when called with an object with a string length attribute", () => {
         expect(isArrayLikeObject({length:"1", 'foo': 2})).to.equal(false);
     })
-    it("throws an error when called with faulty parameters", () => {
-        expect(() => isArrayLikeObject()).to.throw();
-        expect(() => isArrayLikeObject([1,2,3], 2)).to.throw();
-        expect(() => isArrayLikeObject('abc', 'def')).to.throw();
-    })
 })
